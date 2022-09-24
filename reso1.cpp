d105f5796fc2da747d992dbec593dad0d033acb7
scryz <39334189+scryz@users.noreply.github.com>
2022-09-24 09:51:35 +0700

    Create reso1.cpp

---
+++ reso1.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "pch.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int year;
+	cout << "Введите год: ";
+	cin >> year;
+	if (year % 4 == 0) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0)
+				cout << "Год вискосный";
+			else
+				cout << "Год не вискосный";
+		}
+		else
+			cout << "Год вискосный";
+	}
+	else
+		cout << "Год не вискосный";
+	return 0;
+}
